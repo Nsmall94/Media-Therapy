bcec98cea276773f68b357e441dce22614b5579a
daniel natale <dnatale667@gmail.com>
2021-10-05 12:24:15 -0400

    button start

---
+++ assets/js/script.js
@@ -0,0 +1,213 @@
+var btn = document.getElementById("btn");
+
+
+
+$(".health-btn").click(handler)
+//if you have to dynamically update, use onclick: on("click", handler)
+function handler(){
+var bookData = $(this).data("watermelon");
+var dictionaryData = $(this).data("fruit");
+
+}
+
+// console.log(testArray.Problem)
+//api
+var getBookData = function () {
+    var bookApi = "https://api.nytimes.com/svc/books/v3/lists/current/fiction.json?api-key=uIrGoWpSQ12GAANsuzS7wuSArsA9uYPK"
+    fetch(bookApi).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+            
+            
+        });
+    });
+
+};
+var getLocalBookData = function (something) {
+
+for (var i = 0; i < testArray.length; i++){
+    if(something = testArray[i].Problem){
+        console.log(testArray[i].Problem);
+
+    }
+}
+   
+   
+   
+
+};
+//book as object with depression anxiety etc
+
+getLocalBookData("Anxiety");
+
+getBookData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var testArray = 
+// [
+   
+//     {
+//     Books: ["sad book 1", "sad book 2"],
+//     Author: ["sad guy", "sad girl"],
+//     Description: "Ahebvasefv. ",
+//     Problem: "Anxiety"
+//     },
+    
+//     {
+//     Books: ["sad book 1", "sad book 2"],
+//     Author: ["sad guy", "sad girl"],
+//     Description: "Ah, depression; everyone's favorite mental disorder. ",
+//     Problem: "Depression"
+//     },
+// ];
+// problemArray = 
+// [
+//     {
+//         problem: "anxiety",
+//         tag: "hardcover fiction"
+
+//     },
+//     {
+//         problem: "Depression",
+//         tag: "something for the api",
+//         tag2: "something for the other api"
+        
+//     }
+    
+// ];
+
+
+
+
+
+
+
+    // var userInputEl = document.querySelector("#user-form");
+// var weatherInput = document.getElementById("#city");
+// var city = document.querySelector("#city");
+// var weatherBlockEl = document.querySelector("#weather-block-title");
+// var humidityEl = document.querySelector("#humid");
+// var tempEl = document.querySelector("#temperature");
+// var windEl = document.querySelector("#windd");
+// var buttons = document.querySelector("#city-buttonn");
+// btns = document.getElementsByClassName("city-button");
+// var daily = document.querySelector(".daily");
+// var tempTwo = document.querySelector(".tempOne");
+// // var tempppEl = document.getElementsById("#dailyTwo");
+
+// //button input
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function () {
+//         var text = this.textContent;
+
+//         console.log(text);
+//         getWeatherData(text);
+        
+//     });
+// }
+
+//text input
+// function userInputHandler(event) {
+//     event.preventDefault();
+//     var cityData = city.value.trim();
+//     if (cityData) {
+//         city.value = "";
+//         getWeatherData(cityData);
+
+
+//         // console.log(cityData)
+//         // console.log(city.value);
+
+//     }
+//     else {
+// alert("wrong");
+//     }
+
+// }
+
+// userInputEl.addEventListener("submit", userInputHandler);
+
+
+
+//api
+
+// daily.textContent = "";
+
+//makes blocks + data
+// var createFourDay = function(weatherr){
+//    console.log(weatherr.list);
+//    $("#daily").empty();
+//    for(i = 0; i < weatherr.list.length; i++){
+       
+//        var title = weatherr.list[i].dt_txt;
+    
+//     var tempData = weatherr.list[i].main.temp;
+//     var windData = weatherr.list[i].wind.speed;
+//     var humidData = weatherr.list[i].main.humidity;
+//     console.log(title)
+      
+//     var tempppEl = document.createElement("div");
+//     tempppEl.classList = "col align-self-end dailyTwo"
+  
+       
+//     var titleEl = document.createElement("h4");
+// titleEl.textContent = title;
+
+//        var tempText = document.createElement("span");
+//        tempText.textContent = "temp: " + tempData + " F ";
+//        var windText = document.createElement("span");
+//        windText.textContent = "wind Speed: " + windData + " MPH "
+       
+//        var humidityText = document.createElement("span");
+//        humidityText.textContent = "Humidity: " + humidData + "% "
+// tempppEl.appendChild(titleEl);
+//        tempppEl.appendChild(tempText);
+//        tempppEl.appendChild(windText);
+//        tempppEl.appendChild(humidityText);
+//        daily.appendChild(tempppEl);
+
+
+//    }
+// }
+
+
+// var displayWeather = function (weather, city) {
+//     console.log(weather.list[0].main.humidity);
+//     console.log(city);
+//     console.log(weather.city.name);
+    
+//     weatherBlockEl.textContent = weather.city.name + " " + weather.list[0].dt_txt;
+
+//     humidityEl.textContent = "";
+//     var humidityy = weather.list[0].main.humidity;
+//     var humidEl = document.createElement("span");
+//     humidEl.textContent = humidityy + "%";
+//     humidityEl.appendChild(humidEl);
+
+//     tempEl.textContent = "";
+//     var temperaturee = weather.list[0].main.temp;
+//     var tempyyEl = document.createElement("span");
+//     tempyyEl.textContent = temperaturee + " F";
+//     tempEl.appendChild(tempyyEl);
+
+//     windEl.textContent = "";
+//     var windd = weather.list[0].wind.speed;
+//     var windyEl = document.createElement("span");
+//     windyEl.textContent = windd + " mph";
+//     windEl.appendChild(windyEl);
+
+
+// }
